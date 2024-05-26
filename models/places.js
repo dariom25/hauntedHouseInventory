78906087481a7766630d2b25a6ec212d1ffe576e
dariom25 <dario@maciey.de>
2024-05-26 12:00:21 +0200

    Add Schema for places

---
+++ models/places.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema();
+
+const PlaceSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+});
+
+PlaceSchema.virtual("url").get(function () {
+  return `/catalog/place/${this._id}`;
+});

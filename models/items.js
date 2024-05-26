aa6266a334688f1b2a292ca9f1677fdf1ae70a40
dariom25 <dario@maciey.de>
2024-05-26 11:22:20 +0200

    Create the ItemSchema

---
+++ models/items.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const ItemSchema = new Schema({
+    name: {type: String, required: true, maxLength: 100, minLength: 1},
+    summary: {type: String, required: true}, 
+    place: {type: Schema.Types.ObjectId, ref: "Place", required: true},
+    finder: {type: Schema.Types.ObjectId, ref: "Finder", required: true},
+    category: {type: Schema.Types.ObjectId, ref: "Category", required: true}
+})
+
+ItemSchema.virtual("url").get(function() {
+    return `/catalog/item/${this._id}`
+})

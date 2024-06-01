bdff65604a2c783b0e6e563e12d8cc24485d08af
dariom25 <dario@maciey.de>
2024-06-01 21:27:48 +0200

    Implement finder_list function

--- controllers/finderController.js
+++ controllers/finderController.js
@@ -1,34 +1,36 @@
-const Finder = require("../models/finder")
-const asyncHandler = require("express-async-handler")
+const Finder = require("../models/finder");
+const asyncHandler = require("express-async-handler");
 
 exports.finder_list = asyncHandler(async (req, res, next) => {
-    //send finder list
-})
+  const allFinders = await Finder.find().sort({ last_name: 1 }).exec();
+
+  res.render("finder_list", { title: "Finder List", finder_list: allFinders });
+});
 
 exports.finder_detail = asyncHandler(async (req, res, next) => {
-    //send finder details
-})
+  //send finder details
+});
 
 exports.finder_create_get = asyncHandler(async (req, res, next) => {
-    //send create form
-})
+  //send create form
+});
 
 exports.finder_create_post = asyncHandler(async (req, res, next) => {
-    //send data from form
-})
+  //send data from form
+});
 
 exports.finder_delete_get = asyncHandler(async (req, res, next) => {
-    //send delete form
-})
+  //send delete form
+});
 
 exports.finder_delete_post = asyncHandler(async (req, res, next) => {
-    //delete data
-})
+  //delete data
+});
 
 exports.finder_update_get = asyncHandler(async (req, res, next) => {
-    //send update form
-})
+  //send update form
+});
 
 exports.finder_update_post = asyncHandler(async (req, res, next) => {
-    //send data from form and update entry
-})
+  //send data from form and update entry
+});

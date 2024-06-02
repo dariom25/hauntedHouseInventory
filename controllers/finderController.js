87e6f18065b20aaf5fd70d55c35c01b882ed8728
dariom25 <dario@maciey.de>
2024-06-02 10:29:18 +0200

    Implement finder_create_post function

--- controllers/finderController.js
+++ controllers/finderController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require("express-validator");
 const Finder = require("../models/finder");
 const asyncHandler = require("express-async-handler");
 
@@ -23,9 +24,53 @@ exports.finder_create_get = asyncHandler(async (req, res, next) => {
   res.render("finder_form", { title: "Create finder" });
 });
 
-exports.finder_create_post = asyncHandler(async (req, res, next) => {
-  //send data from form
-});
+exports.finder_create_post = [
+  body("first_name")
+    .trim()
+    .escape()
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("last_name")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Last name must not be empty.")
+    .isLength({ max: 100 })
+    .withMessage("Last name may not be longer than 100 characters.")
+    .escape(),
+  body("birth", "Invalid date of birth")
+    .optional({ values: false })
+    .isISO8601()
+    .toDate(),
+  body("death", "Invalid date of death")
+    .optional({ values: false })
+    .isISO8601()
+    .toDate(),
+  body("cause_death").trim().escape(),
+  body("found_items").trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const finder = new Finder({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      date_of_birth: req.body.birth,
+      date_of_death: req.body.death,
+      no_of_found_items: req.body.found_items,
+      cause_of_death: req.body.cause_death,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("finder_form", {
+        title: "Create finder",
+        errors: errors.array(),
+      });
+    } else {
+      await finder.save();
+      res.redirect(finder.url);
+    }
+  }),
+];
 
 exports.finder_delete_get = asyncHandler(async (req, res, next) => {
   //send delete form

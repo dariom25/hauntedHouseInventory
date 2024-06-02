c0dbe30a4d25296497eff6fcc69e9971218f175c
dariom25 <dario@maciey.de>
2024-06-02 19:40:10 +0200

    Implement all functions for the place route

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -1,34 +1,124 @@
-const Place = require("../models/places")
-const asyncHandler = require("express-async-handler")
+const Place = require("../models/places");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.place_list = asyncHandler(async (req, res, next) => {
-    //send place list
-})
+  const allplaces = await Place.find().exec();
+
+  res.render("place_list", {
+    title: "place List",
+    place_list: allplaces,
+  });
+});
 
 exports.place_detail = asyncHandler(async (req, res, next) => {
-    //send place details
-})
+  const place = await Place.findById(req.params.id).exec();
+
+  if (place === null) {
+    const err = new Error("place not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("place_detail", { title: place.name, place: place });
+});
 
 exports.place_create_get = asyncHandler(async (req, res, next) => {
-    //send create form
-})
+  res.render("place_form", { title: "Create place" });
+});
 
-exports.place_create_post = asyncHandler(async (req, res, next) => {
-    //send data from form
-})
+exports.place_create_post = [
+  body("name", "Name must not be empty").trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const place = new Place({
+      name: req.body.name,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("place_form", { title: "Create place", errors: errors });
+    } else {
+      await place.save();
+      res.redirect(place.url);
+    }
+  }),
+];
 
 exports.place_delete_get = asyncHandler(async (req, res, next) => {
-    //send delete form
-})
+  const place = await Place.findById(req.params.id).exec();
 
-exports.place_delete_post = asyncHandler(async (req, res, next) => {
-    //delete data
-})
+  if (place === null) {
+    res.redirect("/inventory/places");
+  }
+
+  res.render("place_delete", {
+    title: "Delete Place",
+    place: place,
+  });
+});
+
+exports.place_delete_post = [
+  body("password", "Password must not be empty.").trim().escape(),
+  body("password").equals("secret").withMessage("Enter the correct password"),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const place = await Place.findById(req.params.id).exec();
+
+    if (!errors.isEmpty()) {
+      res.render("place_delete", {
+        title: "Delete Place",
+        place: place,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      await Place.findByIdAndDelete(req.body.placeid);
+      res.redirect("/inventory/places");
+    }
+  }),
+];
 
 exports.place_update_get = asyncHandler(async (req, res, next) => {
-    //send update form
-})
+  const place = await Place.findById(req.params.id).exec();
 
-exports.place_update_post = asyncHandler(async (req, res, next) => {
-    //send data from form and update entry
-})
+  if (place === null) {
+    res.redirect("/inventory/places");
+  }
+
+  res.render("place_update", {
+    title: "Update Place",
+    place: place,
+  });
+});
+
+exports.place_update_post = [
+  body("password", "Password must not be empty.").trim().escape(),
+  body("password").equals("secret").withMessage("Enter the correct password"),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const place = new Place({
+      name: req.body.name,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      const place = await Place.findById(req.params.id).exec();
+
+      res.render("place_update", {
+        title: "Update Place",
+        place: place,
+      });
+    } else {
+      const updatedplace = await Place.findByIdAndUpdate(
+        req.params.id,
+        place,
+        {}
+      );
+      res.redirect(updatedplace.url);
+    }
+  }),
+];

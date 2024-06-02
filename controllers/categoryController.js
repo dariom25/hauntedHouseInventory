9436900e4b3c0d3f662f0391eb586b111959f31f
dariom25 <dario@maciey.de>
2024-06-02 18:54:28 +0200

    Implement category_create_post function

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
   const allCategories = await Category.find().exec();
@@ -23,12 +24,27 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-  res.render("category_form", {title: "Create Category"})
+  res.render("category_form", { title: "Create Category" });
 });
 
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-  //send data from form
-});
+exports.category_create_post = [
+  body("name", "Name must not be empty").trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_form", { title: "Create Category", errors: errors });
+    } else {
+      await category.save();
+      res.redirect(category.url);
+    }
+  }),
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
   //send delete form
